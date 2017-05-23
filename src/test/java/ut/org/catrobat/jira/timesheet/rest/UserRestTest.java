4abf6f0adef58ce036d781c0d8e6a3a6d52f6104
Markus Reiter-Haas <iseratho@gmail.com>
2017-05-23 15:58:00 +0200

    Added UserRest tests

--- src/test/java/ut/org/catrobat/jira/timesheet/rest/UserRestTest.java
+++ src/test/java/ut/org/catrobat/jira/timesheet/rest/UserRestTest.java
@@ -6,14 +6,19 @@ import com.atlassian.jira.bc.JiraServiceContext;
 import com.atlassian.jira.bc.group.search.GroupPickerSearchService;
 import com.atlassian.jira.bc.user.search.UserSearchService;
 import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.exception.PermissionException;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.user.util.UserManager;
 import com.atlassian.jira.user.util.UserUtil;
 import net.java.ao.EntityManager;
 import net.java.ao.test.jdbc.Data;
 import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
+import org.catrobat.jira.timesheet.activeobjects.Timesheet;
+import org.catrobat.jira.timesheet.rest.json.JsonTeamInformation;
+import org.catrobat.jira.timesheet.rest.json.JsonUserInformation;
 import org.catrobat.jira.timesheet.services.*;
 import org.catrobat.jira.timesheet.rest.UserRest;
+import org.catrobat.jira.timesheet.services.impl.TimesheetServiceImpl;
 import org.mockito.Matchers;
 import org.junit.Assert;
 import org.junit.Before;
@@ -40,6 +45,7 @@ import static org.mockito.Mockito.*;
 @PrepareForTest(ComponentAccessor.class)
 public class UserRestTest {
 
+    private TestActiveObjects ao;
     private UserManager userManagerJiraMock;
     private UserRest spyUserRest;
     private HttpServletRequest httpRequestMock;
@@ -49,11 +55,14 @@ public class UserRestTest {
     private PermissionService permissionServiceMock;
     private UserSearchService userSearchService;
     private GroupPickerSearchService groupPickerSearchService;
+    private List<ApplicationUser> usersSet;
+    private ApplicationUser user1;
+    private ApplicationUser user2;
 
     @Before
     public void setUp() throws Exception {
         assertNotNull(entityManager);
-        TestActiveObjects ao = new TestActiveObjects(entityManager);
+        ao = new TestActiveObjects(entityManager);
 
         userManagerJiraMock = mock(UserManager.class, RETURNS_DEEP_STUBS);
         userUtilMock = mock(UserUtil.class, RETURNS_DEEP_STUBS);
@@ -61,17 +70,39 @@ public class UserRestTest {
         httpRequestMock = mock(HttpServletRequest.class, RETURNS_DEEP_STUBS);
         userMock = mock(ApplicationUser.class, RETURNS_DEEP_STUBS);
         permissionServiceMock = mock(PermissionService.class, RETURNS_DEEP_STUBS);
-        TimesheetService timesheetServiceMock = mock(TimesheetService.class, RETURNS_DEEP_STUBS);
+        TimesheetService timesheetService = new TimesheetServiceImpl(ao);
         TimesheetEntryService timesheetEntryServiceMock = mock(TimesheetEntryService.class, RETURNS_DEEP_STUBS);
         TeamService teamService = mock(TeamService.class, RETURNS_DEEP_STUBS);
         userSearchService = mock(UserSearchService.class, RETURNS_DEEP_STUBS);
         groupPickerSearchService = mock(GroupPickerSearchService.class, RETURNS_DEEP_STUBS);
+        UserManager userManager = mock(UserManager.class);
 
-        UserRest userRest = new UserRest(configServiceMock, permissionServiceMock, timesheetServiceMock,
+        UserRest userRest = new UserRest(configServiceMock, permissionServiceMock, timesheetService,
                 timesheetEntryServiceMock, teamService, userSearchService, groupPickerSearchService);
         spyUserRest = spy(userRest);
 
+
+        user1 = mock(ApplicationUser.class);
+        user2 = mock(ApplicationUser.class);
+
+        Mockito.when(user1.getDisplayName()).thenReturn("User 1");
+        Mockito.when(user2.getDisplayName()).thenReturn("User 2");
+        Mockito.when(user1.getName()).thenReturn("User 1");
+        Mockito.when(user2.getName()).thenReturn("User 2");
+
+        usersSet = new ArrayList<>(Arrays.asList(user1, user2));
+        when(userSearchService.findUsersAllowEmptyQuery(Matchers.any(JiraServiceContext.class), Matchers.eq(""))).thenReturn(usersSet);
+
         PowerMockito.mockStatic(ComponentAccessor.class);
+        PowerMockito.when(ComponentAccessor.getUserManager()).thenReturn(userManager);
+        ApplicationUser chris = mock(ApplicationUser.class);
+        ApplicationUser joh = mock(ApplicationUser.class);
+        when(userManager.getUserByKey("chris")).thenReturn(chris);
+        when(userManager.getUserByKey("joh")).thenReturn(joh);
+        when(chris.getUsername()).thenReturn("chris");
+        when(joh.getUsername()).thenReturn("joh");
+        when(chris.getEmailAddress()).thenReturn("chris@example.com");
+        when(joh.getEmailAddress()).thenReturn("joh@example.com");
         Mockito.when(permissionServiceMock.checkUserPermission()).thenReturn(null);
     }
 
@@ -92,18 +123,6 @@ public class UserRestTest {
         Mockito.when(ComponentAccessor.getUserManager()).thenReturn(userManagerJiraMock);
         Mockito.when(ComponentAccessor.getUserUtil()).thenReturn(userUtilMock);
 
-        ApplicationUser user1 = mock(ApplicationUser.class);
-        ApplicationUser user2 = mock(ApplicationUser.class);
-
-        List<ApplicationUser> usersSet = new ArrayList<>(Arrays.asList(user1, user2));
-
-        when(userSearchService.findUsersAllowEmptyQuery(Matchers.any(JiraServiceContext.class), Matchers.eq(""))).thenReturn(usersSet);
-
-        Mockito.when(user1.getDisplayName()).thenReturn("User 1");
-        Mockito.when(user2.getDisplayName()).thenReturn("User 2");
-        Mockito.when(user1.getName()).thenReturn("User 1");
-        Mockito.when(user2.getName()).thenReturn("User 2");
-
         spyUserRest.getUsers(httpRequestMock);
 
         verify(user1, times(1)).getDisplayName();
@@ -116,18 +135,6 @@ public class UserRestTest {
 
         Mockito.when(ComponentAccessor.getUserManager()).thenReturn(userManagerJiraMock);
         Mockito.when(ComponentAccessor.getUserUtil()).thenReturn(userUtilMock);
-
-        ApplicationUser user1 = mock(ApplicationUser.class);
-        ApplicationUser user2 = mock(ApplicationUser.class);
-
-        List<ApplicationUser> usersSet = new ArrayList<>(Arrays.asList(user1, user2));
-
-        when(userSearchService.findUsersAllowEmptyQuery(Matchers.any(JiraServiceContext.class), Matchers.eq(""))).thenReturn(usersSet);
-
-        Mockito.when(user1.getDisplayName()).thenReturn("User 1");
-        Mockito.when(user2.getDisplayName()).thenReturn("User 2");
-        Mockito.when(user1.getName()).thenReturn("User 1");
-        Mockito.when(user2.getName()).thenReturn("User 2");
 
         Group group = mock(Group.class);
         Mockito.when(group.getName()).thenReturn("DISABLED");
@@ -150,5 +157,30 @@ public class UserRestTest {
 
         Response response = spyUserRest.getGroups(httpRequestMock);
         assertEquals(2, ((List<Group>)response.getEntity()).size());
+    }
+
+    @Test
+    public void testUserInformation() {
+        when(permissionServiceMock.checkRootPermission()).thenReturn(null);
+        Response response = spyUserRest.getUserInformation(httpRequestMock);
+        assertEquals(2, ((List<JsonUserInformation>)response.getEntity()).size());
+    }
+
+    @Test
+    public void testTeamInformation() throws PermissionException {
+        when(permissionServiceMock.checkIfUserExists()).thenReturn(userMock);
+        when(permissionServiceMock.isUserTeamCoordinator(userMock)).thenReturn(true);
+        when(permissionServiceMock.isUserCoordinatorOfTimesheet(userMock, ao.find(Timesheet.class,
+                "USER_KEY = ?", "chris")[0])).thenReturn(true);
+        when(permissionServiceMock.isUserCoordinatorOfTimesheet(userMock, ao.find(Timesheet.class,
+                "USER_KEY = ?", "joh")[0])).thenReturn(false);
+        Response response = spyUserRest.getUsersForCoordinator(httpRequestMock);
+        assertEquals(1, ((List<JsonTeamInformation>)response.getEntity()).size());
+    }
+
+    @Test
+    public void testPairProgrammingUsers() {
+        Response response = spyUserRest.getPairProgrammingUsers(httpRequestMock);
+        assertEquals(2, ((List<String>)response.getEntity()).size());
     }
 }
