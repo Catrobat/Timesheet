17cc872315f7bb1827b51755eb04c9f288709266
Clemens Walluschek <clemens.walluschek@student.tugraz.at>
2020-08-18 14:09:29 +0200

    ATLDEV-274 Fixed Test ActivityVerification Auto inactive

--- src/test/java/ut/org/catrobat/jira/timesheet/scheduling/ActivityVerificationJobTest.java
+++ src/test/java/ut/org/catrobat/jira/timesheet/scheduling/ActivityVerificationJobTest.java
@@ -1,8 +1,7 @@
 package ut.org.catrobat.jira.timesheet.scheduling;
 
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
-import static org.mockito.Mockito.mock;
-
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.plugins.osgi.test.Application;
 import org.catrobat.jira.timesheet.activeobjects.Category;
 import org.catrobat.jira.timesheet.activeobjects.Team;
 import org.catrobat.jira.timesheet.activeobjects.Timesheet;
@@ -12,6 +11,7 @@ import org.catrobat.jira.timesheet.scheduling.ActivityVerificationJob;
 import org.catrobat.jira.timesheet.services.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -19,29 +19,37 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.user.UserKeyService;
 import com.atlassian.jira.user.util.UserManager;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.time.ZonedDateTime;
 import java.util.*;
 
+import static org.mockito.Mockito.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ComponentAccessor.class)
 public class ActivityVerificationJobTest {
 
     private ActivityVerificationJob activityVerificationJob;
+    private TimesheetEntryService entryService;
     private SchedulingService schedulingService;
+    private PermissionService permissionService;
     private Map<String, Object> params;
     private UserManager userManagerJiraMock;
+    private ApplicationUser max;
 
     private Timesheet timesheet1;
 
     @Before
-    @PrepareForTest({ComponentAccessor.class})
     public void setUp() {
         activityVerificationJob = new ActivityVerificationJob();
 
         TimesheetService sheetService = Mockito.mock(TimesheetService.class);
-        TimesheetEntryService entryService = Mockito.mock(TimesheetEntryService.class);
+        entryService = Mockito.mock(TimesheetEntryService.class);
         TeamService teamService = Mockito.mock(TeamService.class);
         CategoryService categoryService = Mockito.mock(CategoryService.class);
         schedulingService = Mockito.mock(SchedulingService.class);
+        permissionService = Mockito.mock(PermissionService.class);
         params = new HashMap<>();
         userManagerJiraMock = mock(UserManager.class, RETURNS_DEEP_STUBS);
         UserKeyService userKeyServiceMock = mock(UserKeyService.class, RETURNS_DEEP_STUBS);
@@ -55,6 +63,7 @@ public class ActivityVerificationJobTest {
         params.put("teamService", teamService);
         params.put("categoryService", categoryService);
         params.put("schedulingService", schedulingService);
+        params.put("permissionService", permissionService);
 
         timesheet1 = Mockito.mock(Timesheet.class);
         Timesheet timesheet2 = Mockito.mock(Timesheet.class);
@@ -66,19 +75,24 @@ public class ActivityVerificationJobTest {
 
         List<Timesheet> timesheetList = new ArrayList<>();
         timesheetList.add(timesheet1);
-        timesheetList.add(timesheet2);
-        timesheetList.add(timesheet3);
+        //timesheetList.add(timesheet2);
+        //timesheetList.add(timesheet3);
 
         TimesheetEntry sheet1entry1 = Mockito.mock(TimesheetEntry.class);
         TimesheetEntry sheet1entry2 = Mockito.mock(TimesheetEntry.class);
         TimesheetEntry sheet2entry1 = Mockito.mock(TimesheetEntry.class);
 
+        Team team = mock(Team.class);
         Category category = Mockito.mock(Category.class);
         Mockito.when(category.getName()).thenReturn("asdf");
 
         Mockito.when(sheet1entry1.getCategory()).thenReturn(category);
         Mockito.when(sheet1entry2.getCategory()).thenReturn(category);
         Mockito.when(sheet2entry1.getCategory()).thenReturn(category);
+
+        Mockito.when(sheet1entry1.getTeam()).thenReturn(team);
+
+        when(entryService.getLatestEntry(timesheet1)).thenReturn(sheet1entry1);
 
         TimesheetEntry[] sheet1Entries = {sheet1entry1, sheet1entry2};
         TimesheetEntry[] sheet2Entries = {sheet2entry1};
@@ -89,6 +103,11 @@ public class ActivityVerificationJobTest {
         Mockito.when(entryService.getEntriesBySheet(timesheet2)).thenReturn(sheet2Entries);
         Mockito.when(entryService.getEntriesBySheet(timesheet3)).thenReturn(sheet3Entries);
 
+        max = mock(ApplicationUser.class);
+        when(userManagerJiraMock.getUserByKey("max")).thenReturn(max);
+        when(timesheet1.getUserKey()).thenReturn("max");
+        when(max.getDisplayName()).thenReturn("max");
+
         Set<Team> teamSet = new HashSet<>();
         teamSet.add(Mockito.mock(Team.class));
 
@@ -96,18 +115,14 @@ public class ActivityVerificationJobTest {
     }
 
     @Test
-    public void testExecute() {
-        Mockito.when(timesheet1.getLatestEntryBeginDate()).thenReturn(new Date());
-        activityVerificationJob.execute(params);
-    }
-
     public void testAutoInactive() {
         Mockito.when(timesheet1.getState()).thenReturn(Timesheet.State.ACTIVE);
         ZonedDateTime tooOldDateTime = ZonedDateTime.now().minusDays(15);
         Date tooOld = Date.from(tooOldDateTime.toInstant());
+
         Mockito.when(timesheet1.getLatestEntryBeginDate()).thenReturn(tooOld);
         Mockito.when(schedulingService.isOlderThanInactiveTime(tooOld)).thenReturn(true);
-
+        when(permissionService.isUserInUserGroupDisabled(max)).thenReturn(false);
         activityVerificationJob.execute(params);
 
         Mockito.verify(timesheet1).setState(Timesheet.State.AUTO_INACTIVE);
